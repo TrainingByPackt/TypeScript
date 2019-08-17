f13664663a9446eb0c7799211652b9553d8cc708
jordanhudgens <jordan@devcamp.com>
2019-08-17 18:36:03 -0400

    Added initial error check case from the scoreboard

--- Chapter8/Activity1/activity-solution/scoreboard.ts
+++ Chapter8/Activity1/activity-solution/scoreboard.ts
@@ -1,1 +1,13 @@
-export default class Scoreboard {}
+export default class Scoreboard {
+  homeTeam: String;
+  awayTeam: String;
+
+  constructor(homeTeam, awayTeam) {
+    this.homeTeam = homeTeam;
+    this.awayTeam = awayTeam;
+  }
+
+  render() {
+    return `<div>${this.homeTeam} vs ${this.awayTeam}</div>`;
+  }
+}
--- Chapter8/Activity1/activity-solution/specs/scoreboard.spec.ts
+++ Chapter8/Activity1/activity-solution/specs/scoreboard.spec.ts
@@ -1,30 +1,11 @@
 import Scoreboard from "./../scoreboard.js";
 import "jasmine";
 
-describe("heat map data shape", () => {
-  it("should have an attribute called batterHotZones", () => {
-    const result = Object.keys(data).includes("batterHotZones");
-    expect(result).toBe(true);
-  });
-
-  it("should have an attribute called pitcherHotZones", () => {
-    const result = Object.keys(data).includes("pitcherHotZones");
-    expect(result).toBe(true);
-  });
-
-  it("should have an optional attribute called coordinateMap", () => {
-    const result = Object.keys(data).includes("coordinateMap");
-    expect(result).toBe(true);
-  });
-
-  it("should have a method called findMatch", () => {
-    let jon: hmt.Pitcher;
-
-    jon = {
-      batterHotZones: [[12.2, -3], [10.2, -5], [3, 2]],
-      pitcherHotZones: [[3, 2], [-12.2, 3], [-10.2, 5]]
-    };
-    const result = findMatch(jon.batterHotZones, jon.pitcherHotZones);
-    expect(result).toEqual([[3, 2]]);
+describe("scoreboard with decorator validation", () => {
+  it("should throw an error if the scoreboard's render method isn't passed a JWT token", () => {
+    const scoreboard = new Scoreboard("Blue Jays", "Mariners");
+    expect(function() {
+      scoreboard.render();
+    }).toThrow(new Error("Parsing is not possible"));
   });
 });
