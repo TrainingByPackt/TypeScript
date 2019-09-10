5e32a4a3da5b7fe796356659eeb060f85f8bd6ef
jordanhudgens <jordan@devcamp.com>
2019-09-10 15:31:24 -0600

    Added generated react/ts application to the activity starter

--- Chapter11/Activity1/activity-solution/specs/heatmap.spec.ts
+++
@@ -1,9 +0,0 @@
-import Scoreboard from "./../heatmap.js";
-import "jasmine";
-
-describe("scoreboard with decorator that applies header", () => {
-  it("should wrap supplied content with an h1 header", () => {
-    // TODO
-    // expect(scoreboard.render()).toBe(`<h1>Blue Jays vs Mariners</h1>`);
-  });
-});
