f1c7e90424545f8ee3fc9feaf8f6bc075669dd8f
jordanhudgens <jordan@devcamp.com>
2019-09-13 14:47:33 -0700

    Added in the initial API call to get the heat map data

--- Chapter11/Activity1/activity-solution/HeatMap/src/components/App.tsx
+++ Chapter11/Activity1/activity-solution/HeatMap/src/components/App.tsx
@@ -1,9 +1,29 @@
 import * as React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const batter = require("../assets/batter.png");
 import HeatMapGraph from "./HeatMapGraph";
 
 const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = () => {
+    axios
+      .get("https://api.devcamp.space/heat-map-data")
+      .then(response => {
+        setData(response.data.heat_map_data);
+      })
+      .catch(error => {
+        console.log("Error", error);
+      });
+  };
+
+  // https://api.devcamp.space/heat-map-data
   return (
     <div
       style={{
