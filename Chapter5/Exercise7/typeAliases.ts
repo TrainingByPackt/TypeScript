afd61396cbfc701a0acc42336a3fdcf116cc9b17
Rayon Hunte <rayon.hunte@gmail.com>
2019-12-10 14:16:49 -0400

    type alias

---
+++ Chapter5/Exercise7/typeAliases.ts
@@ -0,0 +1,15 @@
+
+// type alias of type union number literal 
+type countToThree  = 1| 2 | 3;
+
+
+// user type generics to set new type alias to countToThree
+type someCount<T> = {status: string, count: T}
+
+
+// declare new object to of type someCount pass countToThree as generic type argument  
+const start: someCount<countToThree> = {
+    status: 'start', 
+    count: 1
+}
+
