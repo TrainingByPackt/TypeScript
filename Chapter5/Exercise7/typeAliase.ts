79b2fcf3b28a9f95582162e225740793db1eef09
Rayon Hunte <rayon.hunte@gmail.com>
2019-12-16 05:28:53 -0400

    changes to type alais

--- Chapter5/Exercise7/typeAliase.ts
+++ Chapter5/Exercise7/typeAliase.ts
@@ -3,11 +3,17 @@
 type countToThree  = 1| 2 | 3;
 
 
-// user type generics to set new type alias to countToThree
+/* 
+    user type generics to pass a type out object type someCount and set property count
+    to out generic type
+*/  
 type someCount<T> = {status: string, count: T}
 
 
-// declare new object to of type someCount pass countToThree as generic type argument  
+/* 
+declare new object to of type someCount pass countToThree as generic type argument
+count can now only be set to 1, 2 or 3 without compilation errors
+*/  
 const startCount: someCount<countToThree> = {
     status: 'start', 
     count: 1
