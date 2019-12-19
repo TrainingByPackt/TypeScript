fc6eaf061566c0d481a6cbdcd5eb5b2c01a81abd
Rayon Hunte <rayon.hunte@gmail.com>
2019-12-18 21:02:48 -0400

    updates

--- Chapter5/Exercise9/conditionalType.ts
+++ Chapter5/Exercise9/conditionalType.ts
@@ -1,70 +1,4 @@
-// function process<T extends string | null>(
-//     text: T
-//   ): any {
-//     return text === null ? null : text
-//   }
-
-
-// typeof process("foo") //?
-// typeof process(null) //?
-
-
-// type  Cats = {
-//     name: string;
-//     meow():void
-// }
-// type Others = {
-//     name: string;
-// }
-
-
-// // cat example
-// type Animal = Cats | Others
-
-// type extractCat<A> = A extends {meow(): void}? A: never;
-
-// type cat = extractCat<Animal>
-
-// const someCat: cat = {
-//     name: "lion",
-//     meow():void {
-//         console.log("me")
-//     }
-// }
-
-// const someOther: cat = {
-//     name: "zebra",
-//     meow: ():void => {}
-// }
-
-// type Action = {type: "INIT"} | {type: "SYNC"} | {type: "LOGIN", email: string} | {type: "LOGIN_IN_SUCCESS", token: string}
-
-// type ExtractActionParameters<A, T> = A extends {type: T} ? A : never;
-
-// // type ActionType = Action["type"]
-
-// // function dispatch<T extends ActionType>(
-// //     type: T,
-// //     args: ExtractActionParameters<Action, T>
-// //     ): void {}
-
-// type test = ExtractActionParameters<Action, "LOGIN">
-
-// const someTest: test = {
-//     type: 'LOGIN',
-//     email: '@somemail.com'
-// }
-
-
-// const x = 7 < 10 ?  "cool" : "uncool";
-
-// type c <T> = T extends number | string ?  string : null
-// const cType : c<number> = "cool"
-// cType
-
-
 /* 
-
 conditional types allow you to crate not uniform mapping
 how I understand it is, conditionals allow you to apply a type based on a condition 
 using the syntax of the turnery operator 
@@ -98,8 +32,14 @@ const someUserIs: rightUserType<profiles, "admin" > = {
     user: "admin",
     adminProfile: ['reset uer', 'update profiles']
 } 
-
-// console out user to end exercise 
+// console out user
 console.log(someUserIs)
+
+const someOtherUser: rightUserType<profiles, "normal"> = {
+    user: "normal",
+    userProfile: ['some page', 'really cool page']
+}
+// console out user
+console.log(someOtherUser)
 
 
