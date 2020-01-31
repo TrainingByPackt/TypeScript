8ecf46af73e087d6282d959224ed5be7a6356241
Rayon Hunte <rayon.hunte@gmail.com>
2020-01-31 06:39:50 -0400

    updates

--- Chapter7/Exercise2/Inheritance.ts
+++ Chapter7/Exercise2/Inheritance.ts
@@ -1,129 +1,39 @@
-// step 1 super class
-class User {
+type user = {
     userName: string
-    token: string = ''
+    token: string
+    createdAt: number
+}
 
-    constructor(userName: string) {
-        this.userName = userName
-    }
+class User {
+    
+    private userName: string; 
+    private token: string = ''
+    readonly timeStamp: number = new Date().getTime()
 
-    set (token: string) {
+    constructor(userName: string, token: string) {
+        this.userName =  userName
         this.token = token
     }
-    get () {
-        return {
-            userName: this.userName,
-            token: this.token
-        }
-    }
-}
 
-// step 2 instance super class set token 
-// return user object 
-const user = new User('Mark')
-user.set('12345')
-console.log(
-    user.get()
-)
-
-
-// step 3  Extend Super Class User
-// override constructor
-// add new logout method
-class Super extends User {
-   constructor(userName: string){ 
-       if(userName.length > 0) {
-        super(userName)
-       }
-        
-    }
-   logOut(){
+    logOut():void {
         this.userName = ''
         this.token = ''
-   }
-}
-
-// step 4 instance child class Super
-// console out the return from superUser get 
-const superUser = new Super('Rayon')
-// set token
-superUser.set('6789')
-//console out superUser get method return
-console.log(
-    superUser.get()
-)
-// logout 
-superUser.logOut()
-// console out superUser
-console.log(superUser)
-
-// step 5
-// user array
-const myUsers = ['DarkVader','Batman','BlackPanther']
-// new parent class service
-class Service {
-
-    private users: string []
-    protected userCount: number = 0
-    readonly userLimit: number = 100
-
-    constructor(){
-        this.users = []
     }
 
-    get(): string[] {
-        return this.users
-    }
-
-    set(userName: string):void {
-        this.users.push(userName)
-    }
-    
-    getCount(): number {
-        return this.userCount = this.users.length? this.users.length : 0
-    }
-
-    private resetUsers(){
-        this.users = []
-    }
-
-    loadUsers(users: string[]) {
-        this.resetUsers()
-        this.users = [
-            ...users
-        ]
-    }
-
-}
-
-// step 6
-const basicService = new Service()
-console.log(basicService)
-basicService.loadUsers(myUsers)
-console.log(basicService)
-console.log(basicService.getCount())
-console.log(basicService.userLimit)
-
-
-class AddedService extends Service {
-    getUserByIndex(index: number): string {
-        return this.get().length > 0 ? this.get()[index]: 'no users loaded'
-    }
-    AddUser(userName: string){
-        if (userName && userName.length > 0) {
-            this.set(userName)
+    getUser(): user {
+        return {
+            userName: this.userName,
+            token: this.token,
+            createdAt: this.timeStamp
         }
     }
+
+    protected renewToken (newToken: string) {
+        this.token = newToken
+    }
 }
 
-const addedService = new AddedService()
+const user = new User('rayon', '12345678') 
 
-console.log(addedService)
-console.log(addedService.getUserByIndex(0))
-addedService.loadUsers(myUsers)
-console.log(addedService.get(), addedService.userLimit)
-addedService.AddUser('DeathPool')
-console.log(addedService)
-
-
-
+console.log(user)
+console.log(user.getUser())
