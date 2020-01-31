2116671c7a34b6a5f7d2e38c62b057600f700a1a
rayonhunte <rayon.hunte@gmail.com>
2020-01-31 11:48:13 -0400

    update

--- Chapter7/Exmaple1/interface.ts
+++ Chapter7/Exmaple1/interface.ts
@@ -1,58 +1,31 @@
-
-// basic interface
-interface BasicBot {
-    legs: number
-    powerSwitch: boolean
-    turnOn(): boolean
-    turnOff(): boolean
-}
-
-/** 
- * Class 'SecurityBot' incorrectly implements interface 'BasicBot'.
-  Type 'SecurityBot' is missing the following properties from type
-  'BasicBot': legs, powerSwitch, turnOn, turnOff
- * 
-*/
-
-// class SecurityBot implements BasicBot {}
-
-// correct implementation 
-class SecurityBot implements BasicBot {
-    legs = 4;
-    powerSwitch = false;
-
-    turnOn() {
-        return this.powerSwitch = true;
-    }
-
-    turnOff() {
-        return this.powerSwitch = false;
-    }
-}
-
-// new shopping bot with more features
-class ShoppingBot implements BasicBot {
-    legs = 4;
-    powerSwitch = true;
-
-    turnOff(){return this.powerSwitch = false}
-    turnOn(){return this.powerSwitch = true}
-
-    // additional method
-    shop(listOfItems: string[]): string[] {
-        return listOfItems;
-    }
-
+// first interface
+interface ProductTemplate {
+    height: number
+    width: number
+    color: string
 }
 
 
-// instance security bot
-const secBot = new SecurityBot
-console.log(secBot)
+// make product function
+const productMaker = (product: ProductTemplate) => {
+    return product
+} 
 
-// instance shopping bot
-const shopBot = new ShoppingBot
-shopBot.shop(
-    ['bags', 'apples']
+const myProduct: ProductTemplate = {
+    height: 10,
+    width: 12,
+    color: 'red',
+
+}
+
+const wrongProduct = {
+    height: 10
+}
+
+// console.log(
+//      productMaker(wrongProduct)
+// )
+
+console.log(
+    productMaker(myProduct)
 )
-console.log(shopBot)
