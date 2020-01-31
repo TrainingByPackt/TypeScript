897223bccecb532489f5b547bbb9ed78f21e72e8
rayonhunte <rayon.hunte@gmail.com>
2020-01-31 11:57:29 -0400

    updates

--- Chapter7/Exercise2/Inheritance.ts
+++ Chapter7/Exercise2/Inheritance.ts
@@ -1,11 +1,8 @@
-type user = {
-    userName: string
-    token: string
-    createdAt: number
-}
+// not necessary
+// type user = { userName: string, token: string, createdAt: number}
 
+// base / parent class "User"
 class User {
-    
     private userName: string; 
     private token: string = ''
     readonly timeStamp: number = new Date().getTime()
@@ -20,7 +17,7 @@ class User {
         this.token = ''
     }
 
-    getUser(): user {
+    getUser() {
         return {
             userName: this.userName,
             token: this.token,
@@ -31,9 +28,41 @@ class User {
     protected renewToken (newToken: string) {
         this.token = newToken
     }
+
 }
 
-const user = new User('rayon', '12345678') 
+//const user: User = new User('rayon', '12345678') 
+// console.log(user)
+// console.log(user.getUser())
 
-console.log(user)
-console.log(user.getUser())
+
+class Cashier extends User {
+    balance: number = 0
+    float: number = 0
+
+    start(balance: number, float: number): void {
+        this.balance= balance
+        this.float = float
+    }
+}
+
+const cashier: Cashier = new Cashier('max', '12345678')
+
+//cashier.
+
+
+class Inventory extends User {
+    products: string [] = []
+
+    // over ride constructor method, add new prop
+    constructor(userName: string, token: string, products: string[]) {
+        // call parent constructor method
+        super(userName, token)
+        // set new prop
+        this.products = products
+    }
+}
+
+const inventory: Inventory = new Inventory('james', '896458', ['soap', 'matches', 'apples'])
+
+console.log(inventory)
