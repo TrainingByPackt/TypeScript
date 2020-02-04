11b75878308e98ed1ceffc85b41d1936e8bdd5ff
rayonhunte <rayon.hunte@gmail.com>
2020-02-04 16:09:41 -0400

    updates

---
+++ Chapter5/Example1/intersection.ts
@@ -0,0 +1,15 @@
+type Product = {
+    name: string, 
+    price: number,
+    description: string
+} 
+
+let product: Product = {
+    name: 'ball',
+    price: 40,
+    description: 'round red ball'
+}
+
+
+
+
